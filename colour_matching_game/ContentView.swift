243bdb10e75db427eb129fc0bb40ec745f79d316
Bilal Hamza <bilalhamzazuhry@gmail.com>
2026-01-10 12:05:29 +0530

    Initial Commit

---
+++ colour_matching_game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  colour_matching_game
+//
+//  Created by bsccomp242p-026 on 2026-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ colour_matching_game/colour_matching_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  colour_matching_gameApp.swift
+//  colour_matching_game
+//
+//  Created by bsccomp242p-026 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct colour_matching_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
